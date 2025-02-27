474c47480052e28a8971c1af9828db12890f29f4
Biagio Saccone <biagiolavoro63@gmail.com>
2025-02-27 17:36:21 +0100

    Delete Analisi.py

--- Analisi.py
+++
@@ -1,6 +0,0 @@
-df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", 
-                 names=["sepal_length", "sepal_width", "petal_length", "petal_width", "species"])
-
-esplorazione_iniziale(df)
-analisi_approfondita(df)
-visualizzazione(df)
